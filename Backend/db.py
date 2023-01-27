f52f65eda4572b984a95593aa9499a5a523ccba7
Gangadileep <117165065+Gangadileep@users.noreply.github.com>
2023-01-27 17:30:46 +0530

    Update db.py

--- Backend/db.py
+++ Backend/db.py
@@ -6,26 +6,20 @@ mydb = mysql.connector.connect(
     password = "",
     database = "bookstall"
 )
-
-
 mydb_Create_Table_Query = """CREATE TABLE register
 ( fullname varchar(50) not null,
   username varchar(50) not null,
   email varchar(50) not null,
   password varchar(50) not null
- 
 )"""
 mydb_Create_Table_Query = """CREATE TABLE book
-( book_name varchar(50) not null,
+( bookname varchar(50) not null,
   isbn numeric(10) not null,
   author varchar(50) not null,
   category  varchar(50) not null,
   prize numeric(10) not null,
   CONSTRAINT book_pk PRIMARY KEY (isbn)
 )"""
-
-
 cursor = mydb.cursor()
 result = cursor.execute(mydb_Create_Table_Query)
-
 print(" Table created successfully ")
