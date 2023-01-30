6a7de428c5bc425f3f6f9c8c9e67a0031e6db748
Gangadileep <117165065+Gangadileep@users.noreply.github.com>
2023-01-30 17:27:41 +0530

    Update config.py

--- Backend/config.py
+++ Backend/config.py
@@ -6,7 +6,7 @@ from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 mydb = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'bookstall'
+app.config['MYSQL_DATABASE_DB'] = 'bookstore'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
 app.config['JWT_SECRET_KEY'] = 'super-secret'
