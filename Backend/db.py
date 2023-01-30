835f33d04492579c76f0bbc711d320333b20ede8
Gangadileep <117165065+Gangadileep@users.noreply.github.com>
2023-01-30 17:27:08 +0530

    Update db.py

--- Backend/db.py
+++ Backend/db.py
@@ -1,25 +1,25 @@
 import mysql.connector
-
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
     password = "",
-    database = "bookstall"
+    database = "bookstore"
 )
-mydb_Create_Table_Query = """CREATE TABLE register
-( fullname varchar(50) not null,
-  username varchar(50) not null,
-  email varchar(50) not null,
-  password varchar(50) not null
+# TABLE FOR STORING CATEGORY DETAILS OF BOOK
+mydb_Create_Table_Query = """CREATE TABLE category
+(categoryid int(100) not null auto_increment,
+category varchar(50) not null,
+CONSTRAINT category_pk PRIMARY KEY (categoryid)
 )"""
+# TABLE FOR STORING DETAILS OF BOOKS
 mydb_Create_Table_Query = """CREATE TABLE book
-( bookname varchar(50) not null,
-  isbn numeric(10) not null,
-  author varchar(50) not null,
-  category  varchar(50) not null,
-  prize numeric(10) not null,
-  CONSTRAINT book_pk PRIMARY KEY (isbn)
+(bookname varchar(50) not null,
+isbn bigint(13) not null AUTO_INCREMENT PRIMARY KEY,
+author varchar(50) not null,
+category int(100) not null,
+price numeric(50) not null,
+FOREIGN KEY (category) REFERENCES category(categoryid)
 )"""
 cursor = mydb.cursor()
 result = cursor.execute(mydb_Create_Table_Query)
-print(" Table created successfully ")
+print(" Table created successfully")
